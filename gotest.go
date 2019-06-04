901738da8fc0d01561d4c10d19dec4e417a33278
Laura <laura.robertson64@gmail.com>
2019-06-03 22:25:08 -0500

    useful comments added

--- gotest.go
+++ gotest.go
@@ -4,6 +4,12 @@ import ("fmt"
 				"math"
 				"math/rand")
 // fmt is format
+// to import packages inside of packages we use / notation (opposed to dot)
+// to use math & rand we have to import both separately
+
+// use go doc to learn more about a package
+	// example, run in terminal: go doc fmt
+			//or go doc math/rand Intn
 
 func foo() {
 	fmt.Println("The square root of 4 is", math.Sqrt(4))
