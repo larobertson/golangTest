ea766504cb1be97195206c65a81c5806996c3d42
Laura <laura.robertson64@gmail.com>
2019-06-04 17:04:08 -0500

    pointer receivers and bonus session

--- methods.go
+++ methods.go
@@ -5,6 +5,7 @@ import "fmt"
 // two types of of methods
 	// value receivers
 		// just receives values (doesn't need to modify or change anything)
+		// essentially makes a copy, more useful for small structs
 	// pointer receivers
 		// can modify values/attributes in the struct type
 
@@ -18,20 +19,29 @@ const kmhMultiple float64 = 1.60934
 		topSpeedKmh float64
 	}
 
-	func (c car) kmh() float64 {
+	func (c *car) kmh() float64 {
 		// saying type car associates it with the car struct
 		// value receiver
+
+		// c.topSpeedKmh = 500 // <---- see how it changes the end result
 		return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax)
 	}
 
 	func (c car) mph() float64 {
 		// value receiver
+		// c.topSpeedKmh = 500 < ---- see how it changes the end result
 		return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax/kmhMultiple)
 	}
 
 	func (c *car) newTopSpeed(newspeed float64) {
 		// pointer receiver
 		c.topSpeedKmh = newspeed
+	}
+
+	func newerTopSpeed (c car, speed float64) car {
+		// another way to write a method without using value/pointer
+		c.topSpeedKmh = speed
+		return c
 	}
 	
 	func main(){
@@ -49,4 +59,9 @@ const kmhMultiple float64 = 1.60934
 		aCar.newTopSpeed(500)
 		fmt.Println("new speed kmh", aCar.kmh())
 		fmt.Println("new speed mph", aCar.mph())
+		// the newer top speed method
+		aCar = newerTopSpeed(aCar, 500)
+		fmt.Println("new speed diff method kmh", aCar.kmh())
+		fmt.Println("new speed diff method mph", aCar.mph())
+
 	}
