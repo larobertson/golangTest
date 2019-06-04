d5a0c930f53ac7f4e5f30305d20464c99364072b
Laura <laura.robertson64@gmail.com>
2019-06-04 16:26:35 -0500

    struct tut

--- structserver.go
+++ structserver.go
@@ -4,4 +4,19 @@ import "fmt"
 
 type car struct {
 	gasPedal uint16 // anything from 0-65k (65535)
+	brakePedal uint16
+	steeringWheel int16 // -32k to +32k
+	topSpeedKmh float64
+}
+
+func main(){
+	// best way to do this:
+	aCar := car{gasPedal: 22341, 
+							brakePedal: 0, 
+							steeringWheel: 12561, 
+							topSpeedKmh: 225.0}
+	// or do this:
+	// aCar := car{22341,0,12562,225.0}
+
+	fmt.Println(aCar.gasPedal)
 }
