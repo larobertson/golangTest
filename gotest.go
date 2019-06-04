cbe4f40e4997a104ba82693455cef6b3bb33e384
Laura <laura.robertson64@gmail.com>
2019-06-04 11:41:07 -0500

    baby's first function with parameters

--- gotest.go
+++ gotest.go
@@ -11,11 +11,25 @@ import ("fmt"
 	// example, run in terminal: go doc fmt
 			//or go doc math/rand Intn
 
+func add(x float64,y float64) float64 {
+	return x+y
+}
+
 func foo() {
 	fmt.Println("The square root of 4 is", math.Sqrt(4))
 }
 
+func getRandom() {
+	//random is funny in go, unless we redefine our range, it always gives us the same random variable
+	fmt.Println("A number from 1-100", rand.Intn(100))
+}
+
 func main() {
 	foo()
-	fmt.Println("A number from 1-100", rand.Intn(100))
+	getRandom()
+	//define a variable:
+	var num1 float64 = 5.6
+	var num2 float64 = 9.5
+
+	fmt.Println("this is what our add function does:", add(num1,num2))
 }
