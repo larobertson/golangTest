240e3521b0ac5d60b78c58be7ad5f57609683695
Laura <laura.robertson64@gmail.com>
2019-06-04 16:44:10 -0500

    basic pointer method to change speed

--- methods.go
+++ methods.go
@@ -6,6 +6,7 @@ import "fmt"
 	// value receivers
 		// just receives values (doesn't need to modify or change anything)
 	// pointer receivers
+		// can modify values/attributes in the struct type
 
 const usixteenbitmax float64 = 65535
 const kmhMultiple float64 = 1.60934
@@ -19,11 +20,18 @@ const kmhMultiple float64 = 1.60934
 
 	func (c car) kmh() float64 {
 		// saying type car associates it with the car struct
+		// value receiver
 		return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax)
 	}
 
 	func (c car) mph() float64 {
+		// value receiver
 		return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax/kmhMultiple)
+	}
+
+	func (c *car) newTopSpeed(newspeed float64) {
+		// pointer receiver
+		c.topSpeedKmh = newspeed
 	}
 	
 	func main(){
@@ -38,4 +46,7 @@ const kmhMultiple float64 = 1.60934
 		fmt.Println(aCar.gasPedal)
 		fmt.Println(aCar.kmh())
 		fmt.Println(aCar.mph())
+		aCar.newTopSpeed(500)
+		fmt.Println("new speed kmh", aCar.kmh())
+		fmt.Println("new speed mph", aCar.mph())
 	}
