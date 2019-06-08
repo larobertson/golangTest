9ac4be402975a7689cd3dd015836fe9b9a83e904
Laura <laura.robertson64@gmail.com>
2019-06-08 11:21:51 -0400

    getting ready to template

---
+++ gotemplates.go
@@ -0,0 +1,26 @@
+package main
+
+import ("fmt"
+				"net/http"
+				"html/template")
+
+type NewsAggPage struct {
+	Title string
+	News string
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.execute(w, p)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Woah, Go is neat!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
