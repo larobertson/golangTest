6c7c22705350bda35332cd08c169dc6af06c68e5
Laura <laura.robertson64@gmail.com>
2019-06-08 10:28:25 -0400

    simple xml parsing

--- newsagg.go
+++ newsagg.go
@@ -2,12 +2,32 @@ package main
 
 import ("fmt"
 				"net/http"
-				"io/ioutil")
+				"io/ioutil"
+				"encoding/xml")
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+	// That [] is a slice of type Location
+	// xml:"sitemap" references a <sitemap> xml tag
+	// This essentially tells us to extract the value from that tag
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+	// same here as above, we want to parse a <loc> tag
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
